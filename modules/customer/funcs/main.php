4b0b1d283e5a05cc5abe280400896b8db11bdb89
Hồ Ngọc Triển <hongoctrien@2mit.org>
2019-01-28 16:53:58 +0700

    customer: cập nhật sắp xếp

--- modules/customer/funcs/main.php
+++ modules/customer/funcs/main.php
@@ -62,18 +62,18 @@ if (!class_exists('PHPExcel')) {
 
 if ($nv_Request->isset_request('ordername', 'get')) {
     $array_search['ordername'] = $nv_Request->get_title('ordername', 'get');
-    $nv_Request->set_Cookie('ordername', $array_search['ordername']);
-} elseif ($nv_Request->isset_request('ordername', 'cookie')) {
-    $array_search['ordername'] = $nv_Request->get_title('ordername', 'cookie');
+    $nv_Request->set_Cookie($module_data . '_' . $op . '_ordername', $array_search['ordername']);
+} elseif ($nv_Request->isset_request($module_data . '_' . $op . 'ordername', 'cookie')) {
+    $array_search['ordername'] = $nv_Request->get_title($module_data . '_' . $op . 'ordername', 'cookie');
 } else {
     $array_search['ordername'] = 'first_name';
 }
 
 if ($nv_Request->isset_request('ordertype', 'get')) {
     $array_search['ordertype'] = $nv_Request->get_title('ordertype', 'get');
-    $nv_Request->set_Cookie('ordertype', $array_search['ordertype']);
-} elseif ($nv_Request->isset_request('ordername', 'cookie')) {
-    $array_search['ordertype'] = $nv_Request->get_title('ordertype', 'cookie');
+    $nv_Request->set_Cookie($module_data . '_' . $op . 'ordertype', $array_search['ordertype']);
+} elseif ($nv_Request->isset_request($module_data . '_' . $op . 'ordername', 'cookie')) {
+    $array_search['ordertype'] = $nv_Request->get_title($module_data . '_' . $op . 'ordertype', 'cookie');
 } else {
     $array_search['ordertype'] = 'asc';
 }
