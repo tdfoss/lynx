d412da22d6fed2360b1d1a2f6a6f940d666567f6
Nguyễn Hữu Thọ <huuthoqt1994@gmail.com>
2019-05-02 10:50:00 +0700

    product: update code thêm editor cho trường ghi chú.

--- modules/products/funcs/content.php
+++ modules/products/funcs/content.php
@@ -93,6 +93,30 @@ if ($nv_Request->isset_request('submit', 'post')) {
 
 $row['vat'] = !empty($row['vat']) ? $row['vat'] : '';
 
+if (defined('NV_EDITOR')) {
+    require_once NV_ROOTDIR . '/' . NV_EDITORSDIR . '/' . NV_EDITOR . '/nv.php';
+} elseif (!nv_function_exists('nv_aleditor') and file_exists(NV_ROOTDIR . '/' . NV_EDITORSDIR . '/ckeditor/ckeditor.js')) {
+    define('NV_EDITOR', true);
+    define('NV_IS_CKEDITOR', true);
+    $my_head .= '<script type="text/javascript" src="' . NV_BASE_SITEURL . NV_EDITORSDIR . '/ckeditor/ckeditor.js"></script>';
+    
+    function nv_aleditor($textareaname, $width = '100%', $height = '450px', $val = '', $customtoolbar = '')
+    {
+        global $module_data;
+        $return = '<textarea style="width: ' . $width . '; height:' . $height . ';" id="' . $module_data . '_' . $textareaname . '" name="' . $textareaname . '">' . $val . '</textarea>';
+        $return .= "<script type=\"text/javascript\">
+		CKEDITOR.replace( '" . $module_data . "_" . $textareaname . "', {" . (!empty($customtoolbar) ? 'toolbar : "' . $customtoolbar . '",' : '') . " width: '" . $width . "',height: '" . $height . "',});
+		</script>";
+        return $return;
+    }
+}
+$row['note'] = htmlspecialchars(nv_editor_br2nl($row['note']));
+if (defined('NV_EDITOR') and nv_function_exists('nv_aleditor')) {
+    $row['note'] = nv_aleditor('note', '100%', '300px', $row['note'], 'Basic');
+} else {
+    $row['note'] = '<textarea style="width:100%;height:300px" name="note">' . $row['note'] . '</textarea>';
+}
+
 $xtpl = new XTemplate($op . '.tpl', NV_ROOTDIR . '/themes/' . $module_info['template'] . '/modules/' . $module_file);
 $xtpl->assign('LANG', $lang_module);
 $xtpl->assign('MODULE_NAME', $module_name);
