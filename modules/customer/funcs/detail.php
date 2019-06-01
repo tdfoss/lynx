04beb9e970a5f1f83c766bb2e7dcbc0cbb179623
Hồ Ngọc Triển <hongoctrien@tdfoss.vn>
2019-06-01 13:48:07 +0700

    customer: sửa lỗi phân quyền xem khách hàng

--- modules/customer/funcs/detail.php
+++ modules/customer/funcs/detail.php
@@ -16,16 +16,16 @@ if ($nv_Request->isset_request('change_contacts', 'post')) {
     if (empty($id)) {
         die('NO_' . $lang_module['error_no_id']);
     }
-    
+
     $query = 'UPDATE ' . NV_PREFIXLANG . '_' . $module_data . ' SET is_contacts=0 WHERE id=' . $id;
     $db->query($query);
-    
+
     $nv_Cache->delMod($module_name);
     die('OK_' . $lang_module['queue_success']);
 }
 
 $id = $nv_Request->get_int('id', 'post,get', 0);
-$customer_info = $db->query('SELECT * FROM ' . NV_PREFIXLANG . '_' . $module_data . ' t1 INNER JOIN ' . NV_PREFIXLANG . '_' . $module_data . '_share_acc t2 ON t1.id=t2.customerid WHERE id=' . $id . nv_customer_premission($module_name) . ' AND t2.userid=' . $user_info['userid'])->fetch();
+$customer_info = $db->query('SELECT * FROM ' . NV_PREFIXLANG . '_' . $module_data . ' t1 INNER JOIN ' . NV_PREFIXLANG . '_' . $module_data . '_share_acc t2 ON t1.id=t2.customerid WHERE id=' . $id . nv_customer_premission($module_name, 't2.'))->fetch();
 if (!$customer_info) {
     Header('Location: ' . NV_BASE_SITEURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&' . NV_NAME_VARIABLE . '=' . $module_name . '&' . NV_OP_VARIABLE . '=manage_cusomer');
     die();
@@ -57,10 +57,10 @@ $current_link = NV_BASE_SITEURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LAN
 
 if (isset($site_mods['services'])) {
     define('NV_SERVICES', true);
-    
+
     $_sql = 'SELECT * FROM ' . NV_PREFIXLANG . '_services WHERE active=1 ORDER BY weight';
     $array_services = $nv_Cache->db($_sql, 'id', 'services');
-    
+
     $result = $db->query('SELECT * FROM ' . NV_PREFIXLANG . '_services_customer WHERE customerid=' . $id . ' ORDER BY id DESC');
     while ($row = $result->fetch()) {
         $array_customer_service[] = $row;
@@ -70,7 +70,7 @@ if (isset($site_mods['services'])) {
 
 if (isset($site_mods['projects'])) {
     define('NV_PROJECTS', true);
-    
+
     $result = $db->query('SELECT * FROM ' . NV_PREFIXLANG . '_projects WHERE customerid=' . $id . ' ORDER BY id DESC');
     while ($row = $result->fetch()) {
         $row['link_view'] = NV_BASE_SITEURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&amp;' . NV_NAME_VARIABLE . '=projects&amp;' . NV_OP_VARIABLE . '=detail&amp;id=' . $row['id'];
@@ -81,13 +81,13 @@ if (isset($site_mods['projects'])) {
 
 if (isset($site_mods['email'])) {
     define('NV_EMAIL', true);
-    
+
     $result = $db->query('SELECT t1.id, t1.title, t1.addtime, t1.useradd FROM ' . NV_PREFIXLANG . '_email t1 INNER JOIN ' . NV_PREFIXLANG . '_email_sendto t2 ON t1.id=t2.email_id WHERE t2.customer_id=' . $id . ' ORDER BY id DESC');
     while ($row = $result->fetch()) {
         $row['link_view'] = NV_BASE_SITEURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&amp;' . NV_NAME_VARIABLE . '=email&amp;' . NV_OP_VARIABLE . '=detail&amp;id=' . $row['id'];
         $row['addtime'] = nv_date('H:i d/m/Y', $row['addtime']);
         $row['useradd'] = !empty($row['useradd']) ? $workforce_list[$row['useradd']]['fullname'] : $lang_module['system'];
-        
+
         $array_email_list[] = $row;
     }
     $customer_info['count_emails'] = sizeof($array_email_list);
